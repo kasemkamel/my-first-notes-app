a8677d0c110b991932296615d02e8361794ac3a1
kasemkamel <kss251477@gmail.com>
2023-03-08 22:01:41 +0200

    onchange

--- kasem/lib/simple_bloc_observer.dart
+++ kasem/lib/simple_bloc_observer.dart
@@ -2,7 +2,10 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 class SimpleBlocObserver implements BlocObserver {
- 
+  @override
+  void onChange(BlocBase bloc, Change change) {
+    debugPrint('change = $change');
+  }
 
 
 
