de8a156c9500f05d41e4dd46f5cba8486e8fb65e
kasemkamel <kss251477@gmail.com>
2023-03-08 22:21:26 +0200

    center

--- kasem/lib/main.dart
+++ kasem/lib/main.dart
@@ -75,7 +75,8 @@ class _MyHomePageState extends State<MyHomePage> {
         // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
-       // This trailing comma makes auto-formatting nicer for build methods.
+      body: Center(
+      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
